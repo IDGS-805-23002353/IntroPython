0552542fea8ed2647a030e0a1136bcc5b5653747
IDGS-805-23002353 <83085@alumnos.utleon.edu.mx>
2026-01-27 23:31:16 -0600

    MensajesFlash

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request 
- 
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+
+import forms
+
 app=Flask(__name__)
+app.secret_key='clave secreta'
+
+csrf=CSRFProtect()
 
 @app.route("/")
 def index():
@@ -8,9 +15,24 @@ def index():
     lista=["Juan","Pedro","Mario"]
     return render_template("index.html",titulo=titulo,lista=lista)
 
-@app.route("/alumnos")
+@app.route("/alumnos", methods=['GET','POST'])
 def alumnos():
-    return render_template("alumnos.html")
+    mat=0
+    nom=''
+    apa=''
+    ama=''
+    email=''
+    alumnos_class=forms.UserForm(request.form)
+    if request.method=='POST' and alumnos_class.validate:
+        mat=alumnos_class.matricula.data
+        nom=alumnos_class.nombre.data
+        apa=alumnos_class.apaterno.data
+        ama=alumnos_class.amaterno.data
+        email=alumnos_class.correo.data
+        
+        mensaje='Bienvenido {}'.format(nom)
+        flash(mensaje)
+    return render_template("alumnos.html", form=alumnos_class, mat=mat, nom=nom, apa=apa, ama=ama, email=email)
 
 @app.route("/usuarios")
 def usuarios():
@@ -34,9 +56,9 @@ def formulario():
         <input type="text" id="name" name="name" required>
     </form>
 '''
-@app.route("/operasBas")
+@app.route("/operasBas", methods=['GET','POST'])
 def opera1():
-    res = None
+    res = 0
     if request.method=="POST":
      n1= request.form.get("n1")
      n2= request.form.get("n2")
@@ -52,9 +74,7 @@ def resultado():
     return "La suma de {} + {} = {}".format(n1,n2,res)
 
 
-
-
-
 if __name__=='__main__':
+    csrf.init_app(app)
     app.run(debug=True)
     
